4459830d09a2c23c316441953783659749cacdef
marcosRuiz3 <115492789+marcosRuiz3@users.noreply.github.com>
2024-12-15 15:07:56 +0100

    Add files via upload
    Añadiendo archivos

---
+++ BC06/Cliente/Cliente_Presentacion/IU_Cliente.java
@@ -0,0 +1,5 @@
+package Modelo_diseño.Estatico.Cliente.Cliente_Presentacion;
+
+public class IU_Cliente {
+	private String _tipoUsuario;
+}
---
+++ BC06/Servidor/Servidor_Dominio/BBD_Conector.java
@@ -0,0 +1,8 @@
+package Modelo_diseño.Estatico.Servidor.Servidor_Dominio;
+
+public class BBD_Conector {
+
+	public void Conectar_BD() {
+		throw new UnsupportedOperationException();
+	}
+}
