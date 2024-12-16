5289cab757ac06d38a7391d663da680a13971723
marcosRuiz3 <115492789+marcosRuiz3@users.noreply.github.com>
2024-12-16 11:54:54 +0100

    Delete BC06 directory

--- BC06/src/main/java/esi/uclm/BC06/Cliente/Cliente_Dominio/ConectarBD.java
+++
@@ -1,8 +0,0 @@
-package Modelo_diseño.Estatico.Cliente.Cliente_Dominio;
-
-public class ConectarBD {
-
-	public void ConexionBD() {
-		throw new UnsupportedOperationException();
-	}
-}
--- BC06/src/main/java/esi/uclm/BC06/Servidor/Servidor_Dominio/BBD_Conector.java
+++
@@ -1,8 +0,0 @@
-package Modelo_diseño.Estatico.Servidor.Servidor_Dominio;
-
-public class BBD_Conector {
-
-	public void Conectar_BD() {
-		throw new UnsupportedOperationException();
-	}
-}
