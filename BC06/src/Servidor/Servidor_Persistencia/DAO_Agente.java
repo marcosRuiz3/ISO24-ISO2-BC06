be93cc1c6a38d7d10612701b0da48cc11fcd0a6d
Adrii021 <146728133+Adrii021@users.noreply.github.com>
2024-12-16 23:45:00 +0100

    Add files via upload

---
+++ BC06/src/Servidor/Servidor_Persistencia/DAO_Agente.java
@@ -0,0 +1,35 @@
+package It5___Servidor.Servidor_Persistencia;
+
+public class DAO_Agente {
+	private String _mDB;
+	private String _url;
+	private String _driver;
+
+	public void getAgente() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void connect() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void disconnect() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int insert(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int delete(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int update(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public vector select(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+}
